c2547bf53ded0fbac22ce99795feb2a1992b094a
skygd <skygd@compal>
2020-02-13 21:59:52 +0800

    temp分支

--- activeMqProducer/src/main/java/sky/tool/activemq/producer/task/ActivemqConsumer.java
+++ activeMqProducer/src/main/java/sky/tool/activemq/producer/task/ActivemqConsumer.java
@@ -2,6 +2,7 @@ package sky.tool.activemq.producer.task;
 
 import org.apache.log4j.Logger;
 import org.springframework.jms.annotation.JmsListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import sky.tool.activemq.producer.entity.ActiveEntity;
@@ -13,81 +14,51 @@ public class ActivemqConsumer
 	
 	private void goEntity(ActiveEntity entity)
 	{
-		logger.info("id:" + entity.getId() + "\thash:" + entity.getHashcode());
+		//logger.info("id:" + entity.getId() + "\thash:" + entity.getHashcode());
 	}
 	
+	@Async
 	@JmsListener(destination="QueueA")
-	public void qaListnenr(ActiveEntity entity)
+	public void qaListnenr(ActiveEntity entity) throws InterruptedException
 	{
-		try
-		{
-			Thread.sleep(5000l);
-		}
-		catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-		logger.info("队列A处理一条信息");
+		logger.info("队列A处理一条信息\tid:" + entity.getId() + "\thash:" + entity.getHashcode());
+		Thread.sleep(5000l);
 		goEntity(entity);
 	}
 	
+	@Async
 	@JmsListener(destination="QueueB")
-	public void qbListnenr(ActiveEntity entity)
+	public void qbListnenr(ActiveEntity entity) throws InterruptedException
 	{
-		try
-		{
-			Thread.sleep(5000l);
-		}
-		catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-		logger.info("队列B处理一条信息");
+		logger.info("队列B处理一条信息\tid:" + entity.getId() + "\thash:" + entity.getHashcode());
+		Thread.sleep(5000l);
 		goEntity(entity);
 	}
 	
+	@Async
 	@JmsListener(destination="TopicA", containerFactory = "jmsTopicListenerContainerFactory")
-	public void taListnenr(ActiveEntity entity)
+	public void taListnenr(ActiveEntity entity) throws InterruptedException
 	{
-		try
-		{
-			Thread.sleep(5000l);
-		}
-		catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-		logger.info("主题A处理一条信息");
+		logger.info("主题A处理一条信息\tid:" + entity.getId() + "\thash:" + entity.getHashcode());
+		Thread.sleep(5000l);
 		goEntity(entity);
 	}
 	
+	@Async
 	@JmsListener(destination="TopicB", containerFactory = "jmsTopicListenerContainerFactory")
-	public void tbListnenr(ActiveEntity entity)
+	public void tbListnenr(ActiveEntity entity) throws InterruptedException
 	{
-		try
-		{
-			Thread.sleep(5000l);
-		}
-		catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-		logger.info("主题B处理一条信息");
+		logger.info("主题B处理一条信息\tid:" + entity.getId() + "\thash:" + entity.getHashcode());
+		Thread.sleep(5000l);
 		goEntity(entity);
 	}
 	
+	@Async
 	@JmsListener(destination="TopicC", containerFactory = "jmsTopicListenerContainerFactory")
-	public void tcListnenr(ActiveEntity entity)
+	public void tcListnenr(ActiveEntity entity) throws InterruptedException
 	{
-		try
-		{
-			Thread.sleep(5000l);
-		}
-		catch (InterruptedException e)
-		{
-			e.printStackTrace();
-		}
-		logger.info("主题C处理一条信息");
+		logger.info("主题C处理一条信息\tid:" + entity.getId() + "\thash:" + entity.getHashcode());
+		Thread.sleep(5000l);
 		goEntity(entity);
 	}
 }
